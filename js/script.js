2d119c5b608d8f2def746bbd8659f399e3354f33
Karolina Rząsa <karolina.rzasa2002@gmail.com>
2024-02-23 13:25:11 +0100

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
+
+// losowanie ruchu komputera //
+
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -19,3 +22,20 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+// ustalenie ruchu gracza //
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage(('Błąd! Wybierz: 1: kamień, 2: papier, 3: nożyce.'));
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
