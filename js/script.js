67bfb6e3cd2c0cbfa63c50d7fa3e36e7a9fd18d0
Karolina Rząsa <karolina.rzasa2002@gmail.com>
2024-02-23 12:16:37 +0100

    Add var

--- js/script.js
+++ js/script.js
@@ -7,3 +7,8 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+playerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
